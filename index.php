ec1f0b98ee2371b5014fb32ff6e4cb889ecb4672
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2021-02-10 09:58:28 -0500

    Départ

--- index.php
+++ index.php
@@ -13,10 +13,6 @@ Rendre dynamique le code suivant en utilisant des boucles foreach dans une fonct
  * ============================================================================= */
 
 /** @var array $regions  Tableau indexé commençant à 0 contenant les régions retrouvées dans le HTML final */
-$regions = array('Abitibi-Temiscamingue', 'Bas Saint-Laurent', 'Capitale Nationale', 
-	'Centre-du-Québec', 'Chaudière-Appalaches', 'Cote-Nord', 'Estrie', 'Gaspésie-Îles-de-la-Madeleine', 
-	'Lanaudière', 'Laurentides', 'Laval', 'Mauricie', 'Montérégie', 'Montréal', 'Nord-du-Québec', 
-	'Outaouais', 'Saguenay-Lac-Saint-Jean',);
 
 /** fonction affichageMenuJours
  * Retourne un menu déroulant (<select>) contenant les jours de la semaine
@@ -27,22 +23,6 @@ $regions = array('Abitibi-Temiscamingue', 'Bas Saint-Laurent', 'Capitale Nationa
  *   utilisant la méthode longue à plusieurs instructions.
  * @note L'attribut name du select est "jour"
  */
-function affichageMenuJours($courant=0){
-	$jours[] = 'dimanche';
-	$jours[] = 'lundi';
-	$jours[] = 'mardi';
-	$jours[] = 'mercredi';
-	$jours[] = 'jeudi';
-	$jours[] = 'vendredi';
-	$jours[] = 'samedi';
-	$resultat = '<select name="jour">';
-	foreach($jours as $cle=>$jour){
-		$selected = ($courant == $cle) ? ' selected="selected"' : '';
-		$resultat .= '<option value="'.$cle.'"'.$selected.'>'.$jour.'</option>';
-	}
-	$resultat .= '</select>';
-	return $resultat;
-}
 
 /** fonction affichageMenuMois
  * Retourne un menu déroulant (<select>) contenant les mois de l'année
@@ -53,17 +33,6 @@ function affichageMenuJours($courant=0){
  *   la méthode courte à une seule instruction.
  * @note L'attribut name du select est "mois"
  */
-function affichageMenuMois($courant=0){
-	$mois = array(1=>'janvier','février','mars','avril','mai','juin',
-		'juillet','août','septembre','octobre','novembre','décembre');
-	$resultat = '<select name="mois">';
-	foreach($mois as $cle=>$m){
-		$selected = ($courant == $cle) ? ' selected="selected"' : '';
-		$resultat .= '<option value="'.$cle.'"'.$selected.'>'.$m.'</option>';
-	}
-	$resultat .= '</select>';
-	return $resultat;
-}
 
 /** fonction affichageMenu
  * Retourne un menu déroulant (<select>) contenant les valeurs de n'importe 
@@ -75,32 +44,16 @@ function affichageMenuMois($courant=0){
  *   le menu. Défaut = null.
  * @return string - Le HTML du select
  */
-function affichageMenu($nom, $tableau=array(), $courant=0){
-	$resultat = '<select name="'.$nom.'">';
-	foreach($tableau as $cle=>$valeur){
-		$selected = ($courant == $cle) ? ' selected="selected"' : '';
-		$resultat .= '<option value="'.$cle.'"'.$selected.'>'.$valeur.'</option>';
-	}
-	$resultat .= '</select>';
-	return $resultat;
-}
 
 /* =============================================================================
  * EXÉCUTION DU PROCESSUS DE LA PAGE
  * ============================================================================= */
+
 /* Récupération de la donnée 'jour' avec la méthode 1 pour la valeur par défaut. */
-$jourCourant = 3;
-if (isset($_GET['jour'])) {
-	$jourCourant = $_GET['jour'];
-}
+
 /* Récupération de la donnée 'mois' avec la méthode 2 pour la valeur par défaut. */
-if (isset($_GET['mois'])) {
-	$moisCourant = $_GET['mois'];
-} else {
-	$moisCourant = 11;
-}
-/* Récupération de la donnée 'mois' avec la méthode 3 pour la valeur par défaut. */
-$regionCourant = (isset($_GET['region'])) ? $regionCourant = $_GET['region'] : 7;
+
+/* Récupération de la donnée 'region' avec la méthode 3 pour la valeur par défaut. */
 
 /* =============================================================================
  * AFFICHAGE DE LA PAGE
@@ -109,36 +62,73 @@ $regionCourant = (isset($_GET['region'])) ? $regionCourant = $_GET['region'] : 7
  */
 ?><!DOCTYPE html>
 <html lang="fr">
-<head>
-<meta charset="utf-8" />
-<title>Des boucles foreach</title>
-</head>
+	<head>
+		<meta charset="utf-8" />
+		<title>Des boucles foreach</title>
+	</head>
 
-<body>
-<table border="1" style="margin-bottom:1em;">
-	<col span="3" style="width:6em;" />
-	<thead style="background-color:black; color:white;">
-		<tr>
-			<th>Jour<br />courant</th>
-			<th>Mois<br />courant</th>
-			<th>Région<br />courante</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td><?php echo $jourCourant ?></td>
-			<td><?php echo $moisCourant ?></td>
-			<td><?php echo $regionCourant ?></td>
-		</tr>
-	</tbody>
-</table>
-<form action="" method="get">
-<?php 
-echo affichageMenuJours($jourCourant);
-echo affichageMenuMois($moisCourant);
-echo affichageMenu("region", $regions, $regionCourant);
-?>
-<input type="submit" />
-</form>
-</body>
+	<body>
+		<table border="1" style="margin-bottom:1em;">
+			<col span="3" style="width:6em;" />
+			<thead style="background-color:black; color:white;">
+				<tr>
+					<th>Jour<br />courant</th>
+					<th>Mois<br />courant</th>
+					<th>Région<br />courante</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>3</td>
+					<td>11</td>
+					<td>7</td>
+				</tr>
+			</tbody>
+		</table>
+		<form action="" method="get">
+			<select name="jour">
+				<option value="0">dimanche</option>
+				<option value="1">lundi</option>
+				<option value="2">mardi</option>
+				<option value="3" selected="selected">mercredi</option>
+				<option value="4">jeudi</option>
+				<option value="5">vendredi</option>
+				<option value="6">samedi</option>
+			</select>
+			<select name="mois">
+				<option value="1">janvier</option>
+				<option value="2">février</option>
+				<option value="3">mars</option>
+				<option value="4">avril</option>
+				<option value="5">mai</option>
+				<option value="6">juin</option>
+				<option value="7">juillet</option>
+				<option value="8">août</option>
+				<option value="9">septembre</option>
+				<option value="10">octobre</option>
+				<option value="11" selected="selected">novembre</option>
+				<option value="12">décembre</option>
+			</select>
+			<select name="region">
+				<option value="0">Abitibi-Temiscamingue</option>
+				<option value="1">Bas Saint-Laurent</option>
+				<option value="2">Capitale Nationale</option>
+				<option value="3">Centre-du-Québec</option>
+				<option value="4">Chaudière-Appalaches</option>
+				<option value="5">Cote-Nord</option>
+				<option value="6">Estrie</option>
+				<option value="7" selected="selected">Gaspésie-Îles-de-la-Madeleine</option>
+				<option value="8">Lanaudière</option>
+				<option value="9">Laurentides</option>
+				<option value="10">Laval</option>
+				<option value="11">Mauricie</option>
+				<option value="12">Montérégie</option>
+				<option value="13">Montréal</option>
+				<option value="14">Nord-du-Québec</option>
+				<option value="15">Outaouais</option>
+				<option value="16">Saguenay-Lac-Saint-Jean</option>
+			</select>
+			<input type="submit" />
+		</form>
+	</body>
 </html>
